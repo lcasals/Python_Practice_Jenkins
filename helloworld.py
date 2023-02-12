c97e477a45a456504d0abea07e2bf09a02a778bc
Laura Casals <89712188+lcasals@users.noreply.github.com>
2023-02-12 15:16:29 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@
 #import PyPDF2
   
 # creating a pdf file object
-pdfFileObj = open('example.pdf', 'rb')
+pdfFileObj = open('testFile.pdf', 'rb')
   
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
